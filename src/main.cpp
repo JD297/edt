e24125e1e36d12057b7713bdc22e8fa9447f5721
JD297 <jan.dommasch297@gmail.com>
2024-10-29 20:04:37 +0100

    Added command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int main (int argc, char *argv[])
 {
+	if(argc >= 3)
+	{
+		fprintf(stderr, "%s: invalid argument -- \'%s\'\n", TARGET, argv[2]);
+		fprintf(stderr, "usage: %s [path/to/file]\n", TARGET);
+
+		exit(1);
+	}
+	else if(argc == 2) {
+		// TODO load_file
+	}
+
 	return 0;
 }
